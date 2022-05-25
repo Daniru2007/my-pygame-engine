ef20ece74625630c956124517012452c23b3676f
Daniru2007 <daniruayuka@gmail.com>
2022-05-25 12:50:42 +0530

    [FEATURE](PhysicsObject) move method

--- engine/physics.py
+++ engine/physics.py
@@ -18,4 +18,31 @@ class PhysicsObject(object):
         self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
 
     def move(self, movement, tiles):
-        pass
+        collision_types = {
+            "right": False, "left": False,
+            "top": False, "bottom": False
+        }
+        self.x += movement[0]
+        self.rect.x += movement[0]
+        hit_boxes = collide_test(self.rect, tiles)
+        for tile in hit_boxes:
+            if movement[0] > 0:
+                self.rect.right = tile.left
+                collision_types["right"] = True
+            if movement[0] < 0:
+                self.rect.left = tile.right
+            self.x = self.rect.x
+
+        self.y += movement[1]
+        self.rect.y += movement[1]
+        hit_boxes = collide_test(self.rect, tiles)
+        for tile in hit_boxes:
+            if movement[1] < 0:
+                self.rect.top = tile.bottom
+                collision_types["up"] = True
+            if movement[1] > 0:
+                self.rect.bottom = tile.top
+                collision_types["bottom"] = True
+            self.y = self.rect.y
+
+        return collision_types
